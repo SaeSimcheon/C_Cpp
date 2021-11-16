8c9eef096c45ebdd43d88844baf5ac387d4d2a5a
SaeSimCheon <seobqod@gmail.com>
2021-11-17 07:05:08 +0900

    asd

---
+++ with_DH/1117/divide_electronic_net.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void tree(map<int,vector<int>> & _adj,map<int,vector<int>> & _child ,const int & current,const int & parent){
+    for (auto node : _adj[current]){
+        if(node!=parent){
+            _child[current].push_back(node);
+            tree(_adj,_child,node,current);
+        }
+    }
+}
+
+void subtree(vector<int> & _size, map<int,vector<int>> & _child,const int & current){
+    _size[current] =1;
+    for (auto node : _child[current]){
+        subtree(_size,_child,node);
+        _size[current]+=_size[node];
+    }
+}
+
+int solution(int n, vector<vector<int>> wires) {
+    
+    int answer = 101;
+    int candidate = 0;
+    map<int,vector<int>> adj;
+    map<int,vector<int>> child;
+    
+    vector<int> size(n,0);
+    for(auto avec : wires){
+        adj[avec[0]-1].push_back(avec[1]-1);
+        adj[avec[1]-1].push_back(avec[0]-1);
+    }
+    tree(adj,child,n-1,-1);
+    subtree(size,child,n-1);
+    
+    
+    for ( auto i : size){
+        int candidate = abs(n-2*i);
+        if (answer > candidate){
+            answer = candidate;
+        }
+    }
+        
+    return answer;
+}
