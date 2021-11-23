0dbbae8fdce1d02ed80b64b3666d536a03c13b37
SaeSimCheon <seobqod@gmail.com>
2021-11-23 15:16:29 +0000

    addstring and mem size -> issue

--- with_DH/1120/mystring.cpp
+++ with_DH/1120/mystring.cpp
@@ -2,7 +2,7 @@
 */
 
 #include<iostream>
-
+#include<cstring>
 using namespace std;
 class mystring{
     
@@ -15,8 +15,8 @@ class mystring{
     생성자 ... {
     p =  new char[10];
     }
-    
     */
+    int size_mem =0; // 7_p_a
     public :
     /*
          1.인자는 어떤 형태가 적절한가? mystring(const char s[]) 
@@ -32,12 +32,26 @@ class mystring{
         /*
          4. for문을 통한 사이즈 찾기
          for 문을 조건 없이 돌려서 null 문자인 '\0'을 만나는 시점까지 size올려줌
-        */
-        for (int i = 0;;i++){
+         for (int i = 0;;i++){
             if (s[i] =='\0') break;
             size++;
         }
-        p =  new char[size];
+        */
+        /* 
+        8. 사이즈 구하는 방법 반복문 말고 sizeof 이용하는 방법
+        -> cstring의 strlen이라는 함수를 사용하는 방법이 있음
+        -> 아니면 반복문으로 해야할듯
+        */
+        //cout << sizeof()<<endl;
+        //cout << strlen(s)<< endl;
+        //cout << sizeof("delicious")<< endl;
+        
+        size = strlen(s);
+        size_mem = 2*size; // 7_p_a
+        
+        
+        
+        p =  new char[size_mem];
         /* 5. size만큼 반복문을 수행하여
         새로 동적으로 할당받은 영역에 입력
         */
@@ -53,6 +67,39 @@ class mystring{
     */
     void print(){
         cout << p << endl;
+    }
+    
+    
+    /* 7. 뒤에 문자열을 추가하는 함수 addstring 
+    -> p_a. 있는 것 뒤에 새로 메모리를 할당 받아야한다? -> 이런 방식 말고, 애초에 메모리 변화가 있을때마다 현재의 2배를 할당해두고 사용
+    -> p_b. 할당 받은 메모리 크기를 미리 저장해두자. -> size_mem이라는 변수 따로 만들어두기로 함 
+    */
+    
+    mystring & addstring(const char * _added){
+        int added_len = strlen(_added);
+        
+        if (size_mem > size + added_len){    
+            for (int i =0 ; i < added_len; i ++){
+                p[size+i] = _added[i];
+            }
+            size = size+added_len;
+        }else{
+            mystring tmp = *this;// 임시로 객체 저장
+            
+            delete[] p; // p 에 할당된 메모리 해제
+            size_mem = 2*(size+added_len); // 새로 할당받기 위한 메모리 사이즈는 이와 같음
+            p = new char[size_mem]; // p에 새로 할당 받음
+            
+            for (int i =0 ; i < added_len; i ++){
+                p[size+i] = _added[i];
+            }
+            
+            
+            
+        }
+        
+        
+        return *this;
     }
 };
 
