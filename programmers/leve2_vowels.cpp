5cecb3ebee00ec4ba11483bbc0e3225639452a64
SaeSimCheon <seobqod@gmail.com>
2021-11-27 01:05:05 +0900

    vowels init

---
+++ programmers/leve2_vowels.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+/*
+규칙이 있나 -> 개수 패턴이 존재하긴 하는데 몇 번째 단어인지 알아야해서 경우의수를 다 따져볼까 생각했었음
+
+*/
+int solution(string word) {
+    int answer = 0;
+    return answer;
+}
